7f130154d23783329c239834f652a92c920fe96c
KilonzoJames <jay.kilonzo@gmail.com>
2024-10-05 17:08:54 +0300

    Used sql to persist data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # Configure SQLite database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///registrants.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable modification tracking, as it is not needed for SQLite
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Configure Sessions
@@ -19,10 +19,9 @@ session.init_app(app)
 class Registrant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
-    sport = db.Column(db.String(120), unique=True, nullable=False)
+    sport = db.Column(db.String(120), nullable=False)
 
-
-REGISTRANTS = {}
+# REGISTRANTS = {}
 
 SPORTS = ["Baseball", "Basketball", "Hockey"]
 
@@ -32,16 +31,23 @@ def index():
 
 @app.route('/greet', methods=["POST"])
 def greet():
-    # if "name" in request.args:
-    #     name = request.args["name"]
-    # else:
-    #     name = "world"
-        # how to plug the var in the url ?name=David
-    
-    # request.args.get("name", "world")
+    # name = request.args.get("name", "world")
+    # how to plug the var in the url ?name=David
+    # request.args is only used when using get method and post method doesn't show details in the url
     # which retrieves values from query parameters (GET requests).
     name = request.form.get("name", "world")  # Access form data using request.form
     return render_template("greet.html", name = name)
+
+@app.route('/both', methods = ["GET", "POST"])
+def both():
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        if "name" in request.form:
+            name = request.form["name"]
+        else:
+            name = "world"
+        return render_template("greet.html", name = name)
 
 @app.route('/register')
 def register():
@@ -49,37 +55,51 @@ def register():
 
 @app.route('/success', methods=["GET", "POST"])
 def success():
-    #validate submission
     name = request.form.get("name")
     sport = request.form.get("sport")
-    if not name or sport not in SPORTS:
+    # Validate submission
+    if not name or sport not in SPORTS or Registrant.query.filter_by(name=name).first():
         return render_template("failure.html")
-    # Remember registrants
+    # Add registrant to the database
     new_registrant = Registrant(name=name, sport=sport)
     db.session.add(new_registrant)
     db.session.commit()
-    # db.execute("INSERT INTO registrants(name, sport) VALUES(?, ?)", name , sport)
-    REGISTRANTS[name] = sport
+    return redirect("/deregister")
+
+    # REGISTRANTS[name] = sport
     # return render_template("success.html")
-    return redirect("/registrants")
+    # db.execute("INSERT INTO registrants(name, sport) VALUES(?, ?)", name , sport)
 
 @app.route('/registrants')
-def registrants():
-    return render_template("registrants.html", registrants=REGISTRANTS)
+def registrant():
+    registrants = Registrant.query.all()
+    return render_template("registrants.html", registrants=registrants)
 
 @app.route('/radio')
 def radio():
     return render_template("radio.html", sports = SPORTS)
 
-@app.route('/deregister')
+@app.route('/deregister', methods = ["GET", "POST"])
 def deregister():
-    id = request.form.get('id')
-    if id:
-        db.execute("DELETE FROM registrants WHERE id = ?", id)
-    return redirect('/registrants')
-@app.route('/table')
-def table():
-    return render_template("deregister.html")
+    if request.method == "GET":
+        registrants = Registrant.query.all()
+        return render_template("deregister.html", registrants=registrants)
+    elif request.method == "POST":
+        id = request.form.get('id')
+        if id:
+            # Remove registrant by id
+            registrant_to_delete = Registrant.query.get(id)
+            if registrant_to_delete:
+                db.session.delete(registrant_to_delete)
+                db.session.commit()
+                flash(f"Registrant '{registrant_to_delete.name}' has been successfully deregistered.", "success")
+            else:
+                flash("Registrant not found.", "error")
+        else:
+            flash("Invalid ID provided.", "error")
+        return redirect('/deregister')
+        # db.execute("DELETE FROM registrants WHERE id = ?", id)
+
 # Create the database tables
 with app.app_context():
     db.create_all()
